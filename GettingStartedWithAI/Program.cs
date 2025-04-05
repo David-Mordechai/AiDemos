ab790269deb92e1a0c91db3c9b946c75fca27973
David Mordechai <david.mordechai79@gmail.com>
2025-04-06 02:25:36 +0300

    Add OllamaChatDemo and implement AI chat functionality with streaming responses

---
+++ GettingStartedWithAI/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.Ollama;
+
+// reference https://www.youtube.com/watch?v=VAP2ARHkbe4
+
+ServiceCollection serviceCollection = new();
+serviceCollection.AddKernel();
+serviceCollection.AddOllamaChatCompletion("llama3.2", new Uri("http://localhost:11434/"));
+IServiceProvider services = serviceCollection.BuildServiceProvider();
+
+var kernel = services.GetRequiredService<Kernel>();
+kernel.ImportPluginFromType<Demographics>();
+PromptExecutionSettings settings = new OllamaPromptExecutionSettings { FunctionChoiceBehavior = FunctionChoiceBehavior.Auto() };
+
+var chatService = services.GetRequiredService<IChatCompletionService>();
+
+ChatHistory chatHistory = [];
+
+while (true)
+{
+    Console.Write("Q: ");
+    chatHistory.AddUserMessage(Console.ReadLine() ?? string.Empty);
+
+    var assistant = await chatService.GetChatMessageContentsAsync(chatHistory, settings, kernel);
+    chatHistory.AddAssistantMessage(assistant[0].ToString());
+    Console.WriteLine(assistant[0]);
+}
+
+internal class Demographics
+{
+    [KernelFunction]
+    public int GetPersonAge(string name)
+    {
+        return name switch
+        {
+            "David" => 46,
+            "Neria" => 41,
+            "Lior" => 39,
+            _ => 44
+        };
+    }
+}
---
+++ OllamChatDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.AI;
+
+using IChatClient chatClient = new OllamaChatClient(new Uri("http://localhost:11434/"), "llama3.2");
+
+// Start the conversation with context for the AI model
+List<ChatMessage> chatHistory = [];
+
+while (true)
+{
+    // Get user prompt and add to chat history
+    Console.WriteLine("Your prompt:");
+    var userPrompt = Console.ReadLine();
+    chatHistory.Add(new ChatMessage(ChatRole.User, userPrompt));
+
+    // Stream the AI response and add to chat history
+    Console.WriteLine("AI Response:");
+    var response = "";
+    await foreach (var item in
+                   chatClient.GetStreamingResponseAsync(chatHistory))
+    {
+        Console.Write(item.Text);
+        response += item.Text;
+    }
+    chatHistory.Add(new ChatMessage(ChatRole.Assistant, response));
+    Console.WriteLine();
+}
